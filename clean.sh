9cd868a411aba74f00037792bf04811c4d01b725
Randall Dow <randall.dow@fs.utc.com>
2020-05-02 12:28:44 -0700

    Try different css

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -f base-*.html ipad-*.html tech-*.html arete-*.html
+rm -f ipad-*.html tech-*.html arete-*.html
 rm -f technical.html arete.html index.html
 
 find . -name .DS_Store -print0 | xargs -0 rm
