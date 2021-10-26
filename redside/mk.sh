4eba7877722148b7d1a5983d81c459e213d4a713
Randall Dow <randall.dow@fs.utc.com>
2021-10-26 08:24:45 -0700

    Updates

--- redside/mk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#favicon.ico
-#style.css
-#redside-site-logo.png
-
-cat head.html base-index.html tail.html > index.html
--- redside/pub.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -x
-
-rsync -av --delete . randix_ncmx@ssh.phx.nearlyfreespeech.net:.
