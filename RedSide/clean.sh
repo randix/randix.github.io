694ca58e227efd6d1e085e6bdae9cf62b31db73a
Randall Dow <randix@users.noreply.github.com>
2021-10-29 23:28:11 -0700

    move business to randix.net

--- RedSide/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm -f base-*.html ipad-*.html tech-*.html arete-*.html
-rm -f technical.html arete.html index.html
-
-find . -name .DS_Store -print0 | xargs -0 rm
--- RedSide/pub.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-set -x
-
-rsync -av --delete . randix_ncmx@ssh.phx.nearlyfreespeech.net:.
