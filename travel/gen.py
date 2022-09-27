ff235abb2fc164f6fb4f5559ec55bc73e7c8dc1d
Randall Dow <randall.dow@fs.utc.com>
2022-09-27 10:35:47 -0400

    Updates

--- travel/gen.py
+++ travel/gen.py
@@ -15,8 +15,8 @@ rawlist = open("filelist.txt").readlines()
 for line in rawlist:
   files.append(line.strip())
   
-home = '<a href="{SRC}index.html">home</a> &nbsp;'
-up = '<a href="{SRC}travel/index.html">up</a> &nbsp;'
+home = '<a href="{SRC}index.html"><span style="font-size:14px;">home</span></a> &nbsp;'
+up = '<a href="{SRC}travel/index.html"><span style="font-size:14px;">up</span></a> &nbsp;'
 
 for i in range(len(files)):
   if i == 0:
@@ -28,9 +28,9 @@ for i in range(len(files)):
   else: 
     next = re.sub('.txt', '.html', files[i+1])
 
-  prev = '<a href="{SRC}travel/' + prev + '">previous</a> &nbsp;' 
+  prev = '<a href="{SRC}travel/' + prev + '"><span style="font-size:14px;">previous</span></a> &nbsp;' 
   if next != None:
-    next = '<a href="{SRC}travel/' + next +'">next</a>'
+    next = '<a href="{SRC}travel/' + next +'"><span style="font-size:14px;">next</span></a>'
   
   text = open(files[i]).read()
 
